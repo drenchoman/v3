4211298d15fa43836a355827e1dc5504fceb1a93
Oscar Harron <oscarharron@hotmail.com>
2026-03-17 17:35:48 +1300

    fix rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = await getCollection('posts');
+  return rss({
+    title: "O Town - Oscar's Blog",
+    description: 'The personal blog of Oscar Harron',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts${post.id}`,
+    })),
+  });
+}
